d4458e4e0d57323be6df78874076b1bd5fa80daa
ale-barbosa <allessandra_barbosa@outlook.com>
2024-05-30 09:57:02 -0300

    .

--- modelos/pratica.py
+++ modelos/pratica.py
@@ -14,9 +14,19 @@ class Carro:
         self.cor = cor
         self.ano = ano
 
+    def __str__(self):
+        return f'{self.modelo} | {self.cor} | {self.ano}'
+
 carro1 = Carro('Onix', 'Branco', 2013)
 carro2 = Carro('HRV', 'Vinho', 2018)
 carro3 = Carro('Compass', 'Preto e Branco', 2020)
+
+print('Meus futuros carros')
+print(carro1)
+print(carro2)
+print(carro3)
+
+print('------------------------------------')
 
 class Restaurante:
     def __init__(self, nome, categoria, ativo, capacidade, telefone):
@@ -30,4 +40,6 @@ class Restaurante:
         return f'{self.nome} | {self.categoria} | {self.ativo} | {self.capacidade} | {self.telefone}'
 
 restaurante1 = Restaurante('Lanchinho Feliz', 'Lanche', True, 'Rua Lago, 216', 119999999)
+
+print('Restaurantes')
 print(restaurante1)
