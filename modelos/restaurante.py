549bda1578aa99fa301da276dc4e1a9f41864be0
ale-barbosa <allessandra_barbosa@outlook.com>
2024-05-30 10:15:46 -0300

    aprofundando em propriedades

--- modelos/restaurante.py
+++ modelos/restaurante.py
@@ -2,25 +2,30 @@ class Restaurante:
     restaurantes = []
 
     def __init__(self, nome, categoria):
-        self.nome = nome
-        self.categoria = categoria
+        self._nome = nome.title()
+        self._categoria = categoria.upper()
         self._ativo = False
         Restaurante.restaurantes.append(self)
 
     def __str__(self):
-        return f'{self.nome} | {self.categoria}'
+        return f'{self._nome} | {self._categoria}'
     
-    def listar_restaurantes():
+    @classmethod
+    def listar_restaurantes(cls):
         print(f'{'Nome do restaurante'.ljust(20)} | {'Cateroria'.ljust(20)} | {'Status'}')
-        for restaurante in Restaurante.restaurantes:
-            print(f'{restaurante.nome.ljust(20)} | {restaurante.categoria.ljust(20)} | {restaurante.ativo}')
+        for restaurante in cls.restaurantes:
+            print(f'{restaurante._nome.ljust(20)} | {restaurante._categoria.ljust(20)} | {restaurante.ativo}')
 
     @property
     def ativo(self):
         return '☑' if self._ativo else '☐'
+    
+    def alterar_estado(self):
+        self._ativo = not self._ativo
 
-restaurante_praca = Restaurante('Praça', 'Gourmet')
+restaurante_praca = Restaurante('praça', 'Gourmet')
+restaurante_praca.alterar_estado()
 
-restaurante_pizza = Restaurante('Pizza Express', 'Italiana')
+restaurante_pizza = Restaurante('pizza express', 'Italiana')
 
 Restaurante.listar_restaurantes()
